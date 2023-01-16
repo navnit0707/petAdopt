8184d5887da6513bf1c3f11e881be3204acf48da
navnit0707 <knavnit81@gmail.com>
2023-01-17 03:57:01 +0530

    created a form , uses hooks to store state

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  cont[(location, updateLocation)] = useState();
+  const [location, updateLocation] = useState("bihar");
+  const [animal, updateAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
-    <div className="searach-Params">
+    <div className="search-params">
       <form>
         <label htmlFor="location">
           <input
@@ -11,6 +18,45 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              updateAnimal(e.target.value);
+              updateBreed("");
+            }}
+            onBlur={(e) => {
+              updateAnimal(e.target.value);
+              updateBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
